1613e2ade538a3b01073b55632b05242fea655a4
Lawrence Whalen <ldwhalen@gmail.com>
2021-04-20 13:22:01 -0600

    Green intiailize and Red check stock

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -2,14 +2,23 @@ require './lib/food_truck'
 
 RSpec.describe 'FoodTruck' do
   describe '#initialize' do
-    it 'creates a food truck'
+    it 'creates a food truck' do
       food_truck = FoodTruck.new('name')
 
       expect(food_truck.class).to eq(FoodTruck)
     end
-    it 'has a name' do
+    it 'has a name and no inventory' do
       food_truck = FoodTruck.new('name')
 
       expect(food_truck.name).to eq('name')
+      expect(food_truck.inventory).to eq({})
     end
+  end
+  describe '#check_stock' do
+    it 'returns the amount of the item in inventory' do
+      food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+      expect(food_truck.check_stock(item1)).to eq(0)
+    end
+  end
 end
