0360c53aa7a38bb59f8b6aa47395fee866a1e1a2
Lawrence Whalen <ldwhalen@gmail.com>
2021-04-20 13:17:42 -0600

    Green exist red name

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -2,8 +2,14 @@ require './lib/food_truck'
 
 RSpec.describe 'FoodTruck' do
   describe '#initialize' do
-    food_truck = FoodTruck.new('name')
+    it 'creates a food truck'
+      food_truck = FoodTruck.new('name')
 
-    expect(food_truck.class).to eq(FoodTruck)
-  end
+      expect(food_truck.class).to eq(FoodTruck)
+    end
+    it 'has a name' do
+      food_truck = FoodTruck.new('name')
+
+      expect(food_truck.name).to eq('name')
+    end
 end
